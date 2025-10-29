3603d5adc88545befa2213e936e1033ef768c9ae
Jalen Archer <jalen.archer@hotelengine.com>
2025-10-29 14:54:28 -0700

    colorscheme updates and add gitsigns plugin

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,1 +1,12 @@
 -- colorscheme
+
+function Colorize(color)
+  color = color or 'catppuccin-mocha'
+  vim.cmd.colorscheme(color)
+  vim.api.nvim_set_hl(0, 'CurosrLine', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'ColorColumn', { bg = '#17181c' })
+  vim.api.nvim_set_hl(0, 'CursorLineNR', { bold = true, italic = true})
+end
+
+Colorize()
+
